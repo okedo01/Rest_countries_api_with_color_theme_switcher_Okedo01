d77004b779798cde0abbb2750865ddcd41cbc46d
Okedo01 <eliahmwelangi01@gmail.com>
2026-08-21 15:50:37 +0300

    Import Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div>
       const API_KEY = "rc_live_f5ba7797a12a45bea1ec1479875f8082"
+      <p className='bg-amber-500 text-2xl'>malengo</p>
     </div>
   )
 }
