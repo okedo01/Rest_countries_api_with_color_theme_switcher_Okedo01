fa1361594a0187d63008be2428f8be312d0a9545
Okedo01 <eliahmwelangi01@gmail.com>
2026-08-21 18:22:16 +0300

    countryList component

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+
 
 export default function CountryList() {
+  useEffect(() => {
+    fetch("/data.json")
+    .then(res => res.json())
+    .then(data=> console.log(data)
+    )
+    .catch(err => console.log(err)
+    )
+  }, []);
+  
   return (
-    <div>CountryList</div>
+    <div>
+
+    </div>
   )
 }
