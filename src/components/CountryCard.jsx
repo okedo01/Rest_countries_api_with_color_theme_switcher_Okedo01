aa92f8c78c149124d6e3144ec7fca4af874436de
Okedo01 <eliahmwelangi01@gmail.com>
2026-08-22 11:34:15 +0300

    CountryCard component

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -3,41 +3,40 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
-  CardDescription,
-  CardFooter,
+  CardContent,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
 function CountryCard({ country }) {
   return (
-    <Card className="relative mx-auto w-full max-w-sm pt-0">
+    <Card className="country-card relative mx-auto w-full max-w-sm" key={country.name}>
       <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
 
       <img
         src={country.flags.svg}
         alt={`Flag of ${country.name.common}`}
-        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+        className="relative z-20 aspect-video w-full object-cover"
       />
 
       <CardHeader>
-        <CardAction>
-          <Badge variant="secondary">{country.region}</Badge>
-        </CardAction>
-
-        <CardTitle>{country.name.common}</CardTitle>
-
-        <CardDescription>
-          Population: {country.population.toLocaleString()}
-        </CardDescription>
+        <CardTitle>{country.name}</CardTitle>
       </CardHeader>
 
-      <CardFooter>
-        <Button className="w-full">
-          View Details
-        </Button>
-      </CardFooter>
+      <CardContent className="contents">
+        <div className="p-4 text-sm">
+          <p>
+            Population: {country.population.toLocaleString()}
+          </p>
+          <p>
+            Region: {country.region}
+          </p>
+          <p>
+            Capital: {country.capital}
+          </p>
+
+        </div>
+      </CardContent>
     </Card>
   );
 }
