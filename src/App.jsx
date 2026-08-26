8d97d65b22f4ec52b58f92fcdc8ae2b4b728966a
Okedo01 <eliahmwelangi01@gmail.com>
2026-08-26 16:03:27 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import Header from './components/Header'
 import Main from './components/Main'
 import { Route, Routes } from 'react-router-dom'
 import CountryDetails from './components/CountryDetails'
+import Layout from './components/Layout'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/'>
-        <Header />
-        <Main />
-      </Route>
-      <Route path="/country/:code" element={<CountryDetails />} />
-    </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/country/:code" element={<CountryDetails />} />
+        </Route>
+      </Routes>
   )
 }
