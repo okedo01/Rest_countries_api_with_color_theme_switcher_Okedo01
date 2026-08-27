7c118d9e3fda523d06aeef9a8e0633a6bf27c600
Okedo01 <eliahmwelangi01@gmail.com>
2026-08-27 14:21:08 +0300

    country details component

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export default function CountryDetails() {
-    const {code} = useParams();
-    console.log(code);
-    
+  const [countryDetails, setCountryDetails] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const { code } = useParams();
+
+  useEffect(() => {
+    async function getCountryDetails() {
+      try {
+        const response = await fetch("/data.json");
+        if (!response.ok) {
+          throw new Error(`HTTP Error ${response.status}`);
+        }
+        const data = await response.json();
+        const foundCountry = data.find((country) => country.alpha3Code === code);
+        if (!foundCountry) {
+          throw new Error("no country found");
+        }
+        setCountryDetails(foundCountry);
+      } catch (error) {
+        setError("Failed to Load Country Details");
+      }
+    }
+    getCountryDetails();
+  }, [code]);
+
+  if (!countryDetails) {
+    return <p>Loading details data...</p>
+  }
+
   return (
-    <div>CountryDetails</div>
+    <div>
+      {countryDetails.name}
+    </div>
   )
 }
