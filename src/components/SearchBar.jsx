b321d3b5205d4c8665123d1dfaa6e552ef66c5f6
Okedo01 <eliahmwelangi01@gmail.com>
2026-09-09 13:09:39 +0300

    Styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 
 export default function SearchBar({ searchTerm, setSearchTerm }) {
   return (
     <div className='search-bar'>
+      <FontAwesomeIcon icon={faMagnifyingGlass} />
       <input
         type="text"
         placeholder='Search for a country...'
